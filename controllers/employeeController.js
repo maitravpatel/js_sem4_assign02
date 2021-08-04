08ab960822cebb5c8f6c2a78e42ecd94d8c4374d
Maitra Patel <60070105+pm161200@users.noreply.github.com>
2021-08-04 15:07:52 -0400

    30 mins

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
 
 router.get('/',(req,res) => {
     res.render("employee/addOrEdit",{
@@ -8,7 +10,30 @@ router.get('/',(req,res) => {
 });
 
 router.post('/', (req,res) =>{
-    console.log(req.body);
+    insertRecord(req,res);  
 });
 
+
+function insertRecord(req,res){
+    var employee = new Employee();
+    employee.fullName = req.body.fullName;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+    employee.save((err, doc) =>{
+        if(!err)
+        {
+            res.redirect('employee/list');
+        }
+        else{
+            console.log('Error inserting record: ' + err);
+        }
+    } );
+
+}
+
+router.get('/list', (req,res) => {
+    res.json('from List');
+}
+)
 module.exports = router;
